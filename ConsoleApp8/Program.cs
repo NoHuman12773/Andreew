00c8fd588cb5df2a1663a8b1a8df5ce9d8896283
su <su@209-6>
2021-10-05 13:22:08 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите время:");
+                double t = double.Parse(Console.ReadLine());
+                double S = Math.Pow(t, 3) - 3 * Math.Pow(t, 2) + 2;
+                Console.WriteLine("Speed:{0:F2}", S);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
